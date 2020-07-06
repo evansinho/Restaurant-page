76d8acf2962cd8c8bab16fadc068b2d4c6e44e35
evansinho <igiri.evanson@gmail.com>
2020-07-06 11:45:27 +0100

    create elements for the menu page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import navigation from './navBar';
 import showcase from './homePage';
+import menuItem from './menu';
 
 const navbar = navigation();
 const showcaseText = showcase();
+const menuPage = menuItem();
 
 const content = document.querySelector('#content');
 
 content.appendChild(navbar);
 content.appendChild(showcaseText);
+content.appendChild(menuPage);

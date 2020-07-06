8a6d0fff113e7cf64cf5bd3db7958cf4976df8d5
evansinho <igiri.evanson@gmail.com>
2020-07-06 10:43:56 +0100

    add the homepage text elements

---
+++ src/homePage.js
@@ -0,0 +1,21 @@
+const homePage = () => {
+  const home = document.createElement('div');
+  home.classList.add('showcase');
+
+  const textHeader = document.createElement('h1');
+  textHeader.innerText = 'WELCOME TO AFRICAN KITCHEN';
+  const textSmall = document.createElement('p');
+  textSmall.innerText = 'The home of Africa\'s finest cuisines.';
+  const button = document.createElement('button');
+  const buttonText = document.createElement('a');
+  buttonText.innerText = 'VIEW MENU';
+  button.appendChild(buttonText);
+
+  home.appendChild(textHeader);
+  home.appendChild(textSmall);
+  home.appendChild(button);
+
+  return home;
+};
+
+export default homePage;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import navigation from './navBar';
+import showcase from './homePage';
 
 const navbar = navigation();
+const showcaseText = showcase();
 
 const content = document.querySelector('#content');
 
 content.appendChild(navbar);
+content.appendChild(showcaseText);

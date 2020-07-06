939906bb1d6833e00ec8e2c1cb6b9ad323ed62b6
evansinho <igiri.evanson@gmail.com>
2020-07-06 13:06:05 +0100

    add the contact element

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import navigation from './navBar';
 import showcase from './homePage';
 import menuItem from './menu';
+import contactUs from './contact';
 
 const navbar = navigation();
 const showcaseText = showcase();
 const menuPage = menuItem();
+const contactPage = contactUs();
 
 const content = document.querySelector('#content');
 
 content.appendChild(navbar);
 content.appendChild(showcaseText);
 content.appendChild(menuPage);
+content.appendChild(contactPage);
